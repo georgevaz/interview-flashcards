e4ae7422b11a245b986c7e8ca8c5f8e6c9090c20
George Vazquez <george.h.vazquez@gmail.com>
2023-04-04 13:53:08 -0400

    card rotation

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -6,29 +6,57 @@ import Questions from '../questions'
 
 export const Card = () => {
     const [flipped, setFlip] = useState(false);
+    const [rotation, setRotation] = useState(0)
+
+    const handleFlip = (e) => {
+        setFlip(!flipped);
+    }
+
+    const handleRotate = (e, data) => {
+        let degrees = data.x < 0 ? Math.max(data.x * .25, -25) : Math.min(data.x * .25, 25);
+
+        setRotation(degrees);
+    }
+
+    const resetRotate = () => {
+        setRotation(0);
+    }
+
     return (
         <> 
             <Draggable
-            
+            axis='x'
+            position={{x: 0, y: 0}}
+            onDrag={handleRotate}
+            onStop={resetRotate}
             >
-                <div className='card'>
+                <div 
+                className='card' 
+                >
                     <ReactCardFlip 
                     isFlipped={flipped} 
                     flipDirection='horizontal' 
-                    containerStyle={{height: '100%', width: '100%'}}
+                    containerStyle={{
+                        height: '100%', 
+                        width: '100%',
+                        transform: `rotate(${rotation}deg)`
+                    }}
                     >
-                            <div className='card-front' onClick={() => setFlip(!flipped)}>
+                            <div 
+                            className='card-front' 
+                            onClick={handleFlip}
+                            >
                                 <div className='card-inner'>
-                                    <h1>Tech</h1>
+                                    <h1>What is useRef()?</h1>
                                 </div>
                             </div>
-                            <div className='card-back' onClick={() => setFlip(!flipped)}>
+                            <div 
+                            className='card-back' 
+                            onClick={handleFlip}
+                            >
                                 <div className='card-inner'>
                                     <ul>
-                                    <li>Item</li>  
-                                    <li>Item</li>  
-                                    <li>Item</li>  
-                                    <li>Item</li>  
+                                    <li>I don't know</li>  
                                     </ul>
                                 </div>
                             </div>
--- client/components/CardContainer.jsx
+++ client/components/CardContainer.jsx
@@ -6,21 +6,6 @@ export const CardContainer = () => {
     return (
         <div className='card-container'>
             <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Draggable
-            // axis='x'
-            bounds={{left: -100, top: -100, right: 100, bottom: 100}}
-            >
-                <div style={{backgroundColor: 'green', height: '50px'}}>
-                    <p>POOP</p>
-                </div>
-            </Draggable>
         </div>
     )
 }
