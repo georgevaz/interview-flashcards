aae5886183e8acebda7cc65132b049fffd4857fa
George Vazquez <george.h.vazquez@gmail.com>
2023-04-06 12:28:36 -0400

    Revert "setup for gh pages"
    This reverts commit b2cb5b3dd97ed2156b731cb97445fea4efc57b06.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './docs/index.js',
+    entry: './client/index.js',
     output: {
         path: path.resolve(__dirname, './build'),
         filename: 'bundle.js',
@@ -52,7 +52,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './docs/index.html'
+            template: './client/index.html'
         }),
     ],
     resolve: {
