50b61196afcf99e0e48b4c32882acd0a52cf73a3
George Vazquez <george.h.vazquez@gmail.com>
2023-04-17 19:47:00 -0400

    formatting and style changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,63 +2,63 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js',
-        publicPath: '/'
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  mode: 'development',
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, './build'),
+      publicPath: '/'
     },
-    mode: 'development',
-    devServer: {
-        host: 'localhost',
-        port: 8080,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-        static: {
-            directory: path.join(__dirname, './build'),
-            publicPath: '/'
-        },
-        proxy: {
-            '/api': 'http://localhost:3000'
-        },
+    proxy: {
+      '/api': 'http://localhost:3000'
     },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /(node_modues)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                exclude: /(node_modues)/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modues)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modues)/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './client/index.html'
-        }),
+      },
     ],
-    resolve: {
-        extensions: [
-            '.js',
-            '.jsx'
-        ],
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './client/index.html'
+    }),
+  ],
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ],
+  },
 };
