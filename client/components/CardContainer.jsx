028f2bbe90512404a28f809f9624d03433d3caa7
George Vazquez <george.h.vazquez@gmail.com>
2023-04-17 14:24:19 -0400

    added shuffler

--- client/components/CardContainer.jsx
+++ client/components/CardContainer.jsx
@@ -4,9 +4,30 @@ import useCardStore from '../store/store';
 import { useStore } from 'zustand';
 import * as Questions from '../questions';
 
-const maxOffset = 8;
+// changed the offset to 1 until I can figure out how to combat a bug with accidentally 
+// selecting cards behind the top card
 
-let questionsKeys = Object.keys(Questions.ReactQuestions);
+const maxOffset = 1;
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+let questionsKeys = shuffle(Object.keys(Questions.ReactQuestions));
 let cards = [];
 
 questionsKeys.forEach((x, i) => {
@@ -31,7 +52,7 @@ export const CardContainer = () => {
     } = useStore(useCardStore);
 
     const populateCards = tech => {
-        questionsKeys = Object.keys(Questions[tech]);
+        questionsKeys = shuffle(Object.keys(Questions[tech]));
         cards = [];
         questionsKeys.forEach((x, i) => {
             cards.push(
