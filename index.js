01d44c28c7f1e6b2e02c26b45364369534c92022
George Vazquez <george.h.vazquez@gmail.com>
2023-04-17 22:16:20 -0400

    installed gh-pages

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 import './styles.scss';
 import App from './App';
 
@@ -8,7 +7,5 @@ const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer);
 
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
+  <App />
 );
