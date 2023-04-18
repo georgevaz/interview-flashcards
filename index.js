9b40258b60ada769caf5319ef06c11e73eb77572
George Vazquez <george.h.vazquez@gmail.com>
2023-04-17 22:06:10 -0400

    trying browser router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import './styles.scss';
 import App from './App';
 
@@ -7,5 +8,7 @@ const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer);
 
 root.render(
-  <App />
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
